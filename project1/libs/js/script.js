2078517464b3f6e2919aaed5665e301d7e993645
shoma <shomasdutt@gmail.com>
2021-05-11 15:43:35 +0100

    opencage

--- project1/libs/js/script.js
+++ project1/libs/js/script.js
@@ -1,9 +1,11 @@
-$(window).on('load', function () {
-	if ($('#preloader').length) {
-			$('#preloader').delay(100).fadeOut('slow', function () {
-				$(this).remove();
-			});
-	}
+$(window).on("load", function () {
+  if ($("#preloader").length) {
+    $("#preloader")
+      .delay(100)
+      .fadeOut("slow", function () {
+        $(this).remove();
+      });
+  }
 });
 
 $(document).ready(function () {
@@ -18,19 +20,19 @@ $(document).ready(function () {
       //3
 
       let userLat = position.coords.latitude;
-      let userLong = position.coords.longitude;
+      let userLng = position.coords.longitude;
 
       // console.log(userLat);
       // console.log(userLong);
 
-      let map = L.map("map").setView([userLat, userLong], 13);
+      let map = L.map("map").setView([userLat, userLng], 13);
 
       L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
         attribution:
           '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
       }).addTo(map);
 
-      L.marker([userLat, userLong])
+      L.marker([userLat, userLng])
         .addTo(map)
         .bindPopup("This is your location.")
         .openPopup();
@@ -46,7 +48,7 @@ $(document).ready(function () {
         url: "libs/php/getCountryBorders.php",
         type: "POST",
         dataType: "json",
-        data: "{}",
+        data:  "{}",
 
         success: function (result) {
           //5
@@ -70,129 +72,40 @@ $(document).ready(function () {
               // Use countryBorders.geo.json to set the borders of the countries
 
               L.geoJSON(result.data[i]).addTo(map);
-
-              let borderCoordinates = result.data[i].geometry.coordinates;
-
-              // Create an object with the border and country code
-              let countryAndBorder = {
-                countryCode: result.data[i].properties.iso_a3,
-                border: result.data[i].geometry.coordinates,
-              };
-
-              //console.log(countryAndBorder);
-
-              console.log(countryAndBorder.countryCode);
-
-              
-
-              function nestedLoop(obj) {
-                const res = {};
-                function recurse(obj, current) {
-                  for (const key in obj) {
-                    let value = obj[key];
-                    if (value != undefined) {
-                      if (value && typeof value === "Array") {
-                        recurse(value, key);
-                      } else {
-                        // Do your stuff here to var value
-                        res[key] = value;
-                        console.log(value);
-                      }
-                    }
-                  }
-                }
-                recurse(obj);
-                return res;
-              }
-  
-              nestedLoop(borderCoordinates);
-
-
-
-              // function haversineDistance(
-              //   lat1,
-              //   long1,
-              //   lat2,
-              //   long2,
-              //   unit
-              // ) {
-              //   let radLat1 = (Math.PI * lat1) / 180;
-              //   let radLat2 = (Math.PI * lat2) / 180;
-
-              //   let theta = long1 - long2;
-              //   let radTheta = (Math.PI * theta) / 180;
-
-              //   let dist =
-              //     Math.sin(radLat1) * Math.sin(radLat2) +
-              //     Math.cos(radLat1) *
-              //       Math.cos(radLat2) *
-              //       Math.cos(radTheta);
-
-              //   if (dist > 1) {
-              //     dist = 1;
-              //   }
-
-              //   dist = Math.acos(dist);
-              //   dist = (dist * 180) / Math.PI;
-              //   dist = dist * 60 * 1.1515;
-
-              //   if (unit == "K") {
-              //     dist = dist * 1.609344;
-              //   } // K is kilometres
-              //   if (unit == "N") {
-              //     dist = dist * 0.8684;
-              //   } // N is nautical miles
-              //   return dist;
-              // }
-
-              // // for (let j = 0; j < value[0].length; j++) {
-              // //   let countryBordersLat = value[0][j][0];
-              // //   let countryBordersLong = value[0][j][1];
-              // //   // console.log(countryBordersLat);
-              // // console.log(countryBordersLong);
-
-              // let distance = haversineDistance(
-              //   userLat,
-              //   userLong,
-              //   countryBordersLat,
-              //   countryBordersLong,
-              //   "K"
-              // );
-
-              // //console.log(distance);
-
-              // // Create an array for the distances
-              // let distancesArray = [];
-
-              // // Create an object with the distance and other properties to be retrieved
-              // let distanceObjectArray = [];
-
-              // let distanceObject = {
-              //   distance: distance,
-              //   country: result.data[i].properties.iso_a3,
-              // };
-
-              // distanceObjectArray.push(distanceObject);
-
-              // distancesArray.push(distance);
-
-              // //console.log(distancesArray);
-
-              // //console.log(distanceObjectArray);
-
-              // let closest = Math.min.apply(Math, distancesArray);
-
-              // //console.log(closest);
-
-              // for (let j = 0; j < distanceObjectArray.length; j++) {
-              //   if (distanceObjectArray[j].distance === closest) {
-              //     //console.log(distanceObjectArray[j].country);
-              //   }
-              // }
             } //7
           } //6
         }, //5
       }); //4
+
+
+
+      // opencage
+
+      $.ajax({
+        url: "libs/php/getOpenCage.php",
+        type: 'POST',
+        dataType: 'json',
+        data: {
+          q: encodeURIComponent(userLat + ',' + userLng)
+        },
+        success: function(result) {
+  
+          console.log(JSON.stringify(result));
+  
+          // if (result.status.name == "ok") {
+  
+          //   console.log(result['data'][0].components.country);
+           
+          // }
+        
+        },
+        error: function(jqXHR, textStatus, errorThrown) {
+          // your error code
+        }
+      }); 
+
+
+     
     }); //3
 
     // error: function (jqXHR, textStatus, errorThrown) {
